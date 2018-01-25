948a8006d07a11156703f1b2401898b1423e275c
NemeanLion <maccoleman013@gmail.com>
2018-01-25 11:02:01 -0700

    Specified which forge is which

--- Config.lua
+++ Config.lua
@@ -8,7 +8,7 @@ function GetConfig()
   RedBedCoords = {x = 1, y = 41, z = -69}--coords for the bed
   BlueBedCoords = {x = 1, y = 41, z = 67}
   
-  IronGoldLocate = {{x = 1, y = 41, z = 74}, {x = 1, y = 41, z = -76}} --locations where stuff will spawn
+  IronGoldLocate = {['Blue'] = {x = 1, y = 41, z = 74}, ['Red'] = {x = 1, y = 41, z = -76}} --locations where stuff will spawn
   DiamondLocate = {{x = 1, y = 41, z = 42}, {x = 1, y = 41, z = -39}}
   EmeraldLocate = {{x = -5, y = 42, z = -1}, {x = 7, y = 42, z = -1}}
   
