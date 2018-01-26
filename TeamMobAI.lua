2725f45eaf6ddc45165a679381f59b52377bc7e6
Chen Chai <chen.chai@hotmail.com>
2018-01-25 20:53:36 -0700

    Mobs now only target players not on their team

--- TeamMobAI.lua
+++ TeamMobAI.lua
@@ -1,22 +1,35 @@
 function MobSearchForTarget(Monster, SearchDistance, AttackDistance)
   
+  local ClosestDistance = 1000
   Monster:GetWorld():ForEachPlayer(function(Player)
   
                                     local VectorSum = Monster:GetPosition() - Player:GetPosition()
                                     -- XZ pythagorean distance
                                     local Distance = VectorSum:Length()
                                     
-                                    
-                                    if Distance <= SearchDistance then
-                                      Monster:MoveToPosition(Player:GetPosition())
-                                      LOG(Monster:GetUniqueID() .. " is tracking " .. Player:GetName() .. " at " .. Distance .. " range!")
+                                    if ClosestDistance > Distance then 
+                                      ClosestDistance = Distance
+                                    else
+                                      return
                                     end
                                     
-                                    if Distance <= AttackDistance then
-                                      Player:TakeDamage(dtEntityAttack, Monster, 8, 20)
-                                    end
+                                    if Monster.Team == nil then return end
+                                    if Player:GetTeam == nil then return end
                                     
---                                    if Player:GetTeam():GetName ~= Entity:GetTeam():GetName()
+                                    
+                                      -- Comparing team names should be more reliable than comparing the whole team object
+                                    if Player:GetTeam().GetName() ~= Entity:GetTeam():GetName() then
+                                    
+                                      if Distance <= SearchDistance then
+                                        Monster:MoveToPosition(Player:GetPosition())
+                                        LOG(Monster:GetUniqueID() .. " is tracking " .. Player:GetName() .. " at " .. Distance .. " range!")
+                                      end
+                                    
+                                      if Distance <= AttackDistance then
+                                        Player:TakeDamage(dtEntityAttack, Monster, 8, 20)
+                                      end
+                                      
+                                    end
                                   end)
   return
   
